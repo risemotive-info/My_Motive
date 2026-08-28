34b034eec6547df03dbb7262b2f63310ccf93f44
mugisha frank <iamfrank580@gmail.com>
2026-08-28 12:00:22 +0200

    Remove SQL migrations, will apply via one-time script instead

--- migrations/transactions_approval_workflow.sql
+++
@@ -1,8 +0,0 @@
--- Adds approval workflow columns to the transactions table.
-
-ALTER TABLE transactions
-  ADD COLUMN recorded_by INT(11) NULL,
-  ADD COLUMN status VARCHAR(20) NOT NULL DEFAULT 'approved',
-  ADD COLUMN approved_by INT(11) NULL,
-  ADD COLUMN approved_at DATETIME NULL,
-  ADD COLUMN rejection_reason TEXT NULL;
